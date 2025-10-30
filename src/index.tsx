ef750091383f7d2f820330894611f65d48195ac4
Michał Szwindowski <github@nedi.me>
2025-10-30 06:46:43 +0100

    next

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import 'app/styles/global.scss';
+import './app/styles/global.scss';
 
 import { App } from './app/App';
 
